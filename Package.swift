dd0b6e70b9e7f696ff36e95628ff1a28eb8ddcd3
Matt Gioe <mattgioe@Matts-MacBook-Air.local>
2017-11-19 15:09:27 -0500

    added swiftybeaver

--- Package.swift
+++ Package.swift
@@ -12,9 +12,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/vapor-community/postgresql-provider.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor-community/swiftybeaver-provider.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider"],
+        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider", "SwiftyBeaverProvider"],
                 exclude: [
                     "Config",
                     "Public",
