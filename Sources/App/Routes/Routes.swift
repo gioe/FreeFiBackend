d40da4fcf7256e29397a8453affe88fda8a84596
Matt Gioe <mattgioe@Matts-MacBook-Air.local>
2017-12-28 08:43:50 -0500

    adding routes to filter by id

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -9,6 +9,20 @@ extension Droplet {
             return "Safe"
         }
         
+        
+        get("place", Int.parameter) { request in
+            let parameter = try request.parameters.next(Int.self)
+            let spotToUpdate = try Spot.all().filter{ $0.id == Identifier(parameter) }
+            
+            guard !spotToUpdate.isEmpty, var firstOption = spotToUpdate.first else {
+                return try Response(status: .badRequest, json: ["message": "Couldn't find a spot with this spot's id."])
+            }
+            
+            var spotJSON = try firstOption.makeJSON()
+            
+            return spotJSON
+        }
+
         get("nearbyPlaces", Int.parameter) { request in
             let parameter = try request.parameters.next(Int.self)
 
@@ -29,7 +43,29 @@ extension Droplet {
             return responseJSON
         }
         
-        post("place", "new") { (request) -> ResponseRepresentable in
+        put("place", "update") { request in
+            guard let bytes = request.body.bytes, let json = try? JSON(bytes: bytes) else {
+                return try Response(status: .badRequest, json: ["message": "Error creating JSON"])
+            }
+            
+            guard let name = json["name"]?.string, let address = json["address"]?.string, let city = json["city"]?.string, let state = json["state"]?.string, let zipCode = json["zipCode"]?.int, let latitude = json["latitude"]?.double, let longitude = json["longitude"]?.double, let id = json["id"]?.int else {
+                return try Response(status: .badRequest, json: ["message": "Couldn't produce a spots. One of the variables was malformed"])
+            }
+            
+            let place = Spot(name: name, address: address, zipCode: zipCode, city: city, state: state, latitude: latitude, longitude: longitude)
+
+            let spotToUpdate = try Spot.all().filter{ $0.id == Identifier(id) }
+           
+            guard !spotToUpdate.isEmpty, var firstOption = spotToUpdate.first else {
+                return try Response(status: .badRequest, json: ["message": "Couldn't find a spot with this spot's id."])
+            }
+            
+            firstOption = place
+            try firstOption.save()
+            return try Response(status: .accepted, json: ["message": "Success"])
+        }
+        
+        post("place", "new") { request in
             
             guard let bytes = request.body.bytes, let json = try? JSON(bytes: bytes) else {
                 return try Response(status: .badRequest, json: ["message": "Error creating JSON"])
